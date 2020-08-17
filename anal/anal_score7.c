1f0c378649c018afa064b38769c7421a40ce4661
Jeffrey Crowell <crowell@bu.edu>
2020-08-16 22:10:11 -0400

    add register array to anal

--- anal/anal_score7.c
+++ anal/anal_score7.c
@@ -8,6 +8,17 @@
 
 #define BIT_RANGE(x, start, size) ((x >> start) & ((1 << size) - 1))
 
+static const char *REGISTERS[] = {
+    "r0", "r1", "r2", "r3",
+    "r4", "r5", "r6", "r7",
+    "r8", "r9", "r10", "r11",
+    "r12", "r13", "r14", "r15",
+    "r16", "r17", "r18", "r19",
+    "r20", "r21", "r22", "r23",
+    "r24", "r25", "r26", "r27",
+    "r28", "r29", "r30", "r31",
+};
+
 static int32_t sign_extend(uint32_t x, uint8_t b) {
     uint32_t m = 1UL << (b - 1);
 
