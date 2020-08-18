b0eca67aaa13a0a9c9e248feda2331a1aa29a4a7
Liran Nuna <liranuna@gmail.com>
2020-08-17 21:29:13 -0700

    Unify anal_op->cond handling

--- anal/anal_score7.c
+++ anal/anal_score7.c
@@ -125,7 +125,6 @@ static void anal32(RAnal *anal, RAnalOp *aop, uint32_t addr, uint32_t insn) {
             aop->eob = true;
             aop->type = BIT_RANGE(insn, 0, 1) ? R_ANAL_OP_TYPE_CALL : R_ANAL_OP_TYPE_JMP;
             aop->cond = CONDITIONALS[BIT_RANGE(insn, 10, 5)];
-            aop->type |= (aop->cond != R_ANAL_COND_AL) * R_ANAL_OP_TYPE_COND;
             aop->jump = addr + sign_extend(((BIT_RANGE(insn, 15, 10) << 9) | BIT_RANGE(insn, 1, 9)) << 1, 20);
             return;
     }
@@ -160,7 +159,6 @@ static void anal16(RAnal *anal, RAnalOp *aop, uint32_t addr, uint16_t insn) {
                     aop->reg = REGISTERS[rA];
                     aop->cond = CONDITIONALS[rD];
                     aop->type = (rA == 3) ? R_ANAL_OP_TYPE_RET : R_ANAL_OP_TYPE_RJMP;
-                    aop->type |= (aop->cond != R_ANAL_COND_AL) * R_ANAL_OP_TYPE_COND;
                     return;
                 case 0x5: // t{cond}!
                     aop->type = R_ANAL_OP_TYPE_CMP;
@@ -170,7 +168,6 @@ static void anal16(RAnal *anal, RAnalOp *aop, uint32_t addr, uint16_t insn) {
                     aop->reg = REGISTERS[rA];
                     aop->cond = CONDITIONALS[rD];
                     aop->type = R_ANAL_OP_TYPE_RCALL;
-                    aop->type |= (aop->cond != R_ANAL_COND_AL) * R_ANAL_OP_TYPE_COND;
                     return;
                 default:
                     aop->type = R_ANAL_OP_TYPE_UNK;
@@ -304,7 +301,6 @@ static void anal16(RAnal *anal, RAnalOp *aop, uint32_t addr, uint16_t insn) {
             aop->eob = true;
             aop->cond = CONDITIONALS[BIT_RANGE(insn, 8, 4)];
             aop->type = R_ANAL_OP_TYPE_JMP;
-            aop->type |= (aop->cond != R_ANAL_COND_AL) * R_ANAL_OP_TYPE_COND;
             aop->jump = addr + (sign_extend(BIT_RANGE(insn, 0, 8), 8) << 1);
             return;
         case 0x5: // ldiu!, rD, imm8
@@ -425,6 +421,8 @@ static int score7_anop(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *buffer, i
     }
 
     op->fail = addr + op->size;
+    op->type |= (op->cond != R_ANAL_COND_AL) * R_ANAL_OP_TYPE_COND;
+
     return op->size;
 }
 
